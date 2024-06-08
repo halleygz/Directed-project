ceb1ee30da9f49fa2341b1ffb9886e3d2901ee46
halleygz <halleluya2013idone@gmail.com>
2024-06-08 12:19:26 +0300

    changed email validator

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { default: isEmail } = require("validator/lib/isemail");
+const {  isEmail } = require("validator");
 const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
